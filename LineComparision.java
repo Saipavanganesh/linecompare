9ff53610c451320df901c7b154285c0791ce92bd
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-12 04:55:40 +0530

    Welcome message

---
+++ LineComparision.java
@@ -0,0 +1,8 @@
+package com.lineComparision;
+
+public class LineComparision {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line comparison computation program");
+    }
+}
+
