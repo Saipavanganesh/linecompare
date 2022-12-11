5aed939357cba491736e72483c1e125d1bd33865
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-12 04:42:44 +0530

    Use case 1

--- LineComparision.java
+++ LineComparision.java
@@ -4,8 +4,20 @@ import java.util.Scanner;
 import java.lang.Math;
 public class LineComparision {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparision Computation Program");
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter first co-ordinates:");
+        System.out.println("Enter x1 value:");
+        double x1 = sc.nextDouble();
+        System.out.println("Enter y1 value:");
+        double y1 = sc.nextDouble();
+        System.out.println("Enter x2 value:");
+        double x2 = sc.nextDouble();
+        System.out.println("Enter y2 value:");
+        double y2 = sc.nextDouble();
+
+        double length = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+        System.out.println(length);
     }
 }
 
